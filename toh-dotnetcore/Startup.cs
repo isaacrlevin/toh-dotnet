ab764dc127c499c9966f932278b47a018fc3a08a
Isaac <isaac.r.levin>
2019-09-18 09:55:50 -0400

    cleanup

--- toh-dotnetcore/Startup.cs
+++ toh-dotnetcore/Startup.cs
@@ -62,7 +62,7 @@ namespace toh_dotnetcore.AngularApi
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                //app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
