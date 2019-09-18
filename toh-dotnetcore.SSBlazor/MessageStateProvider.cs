c97391f419167bb5361ff70c0a58cd0674193560
Isaac <isaac.r.levin>
2019-09-18 11:32:47 -0400

    added server side blazor

---
+++ toh-dotnetcore.SSBlazor/MessageStateProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace toh_dotnetcore.SSBlazor
+{
+    public class MessageState
+    {
+        public List<string> Messages { get; private set; } = new List<string>();
+        public void AddMesage(string message)
+        {
+            Messages.Add($"HeroService: ${message}");
+        }
+        public void Clear()
+        {
+            Messages.Clear();
+        }
+    }
+}
---
+++ toh-dotnetcore.SSBlazor/Startup.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using tohdotnetcore.domain;
+using tohdotnetcore.domain.Models;
+
+namespace toh_dotnetcore.SSBlazor
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IHeroService, HeroService>();
+            services.AddScoped<MessageState>();
+
+            services.AddWebEncoders();
+            services.AddApplicationInsightsTelemetry();
+
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+
+            services.AddSnapshotCollector();
+            services.AddDbContext<tohdotnetcoreContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("tohdotnetcoreContext")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
